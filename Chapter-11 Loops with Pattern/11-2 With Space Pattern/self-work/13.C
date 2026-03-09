cf87071a7a360e4a5a93a0214d57df711b95ec7a
Drashti Thummar <thummard097@gmail.com>
2026-03-09 22:11:15 +0530

    Chapter - 11 Patterns

---
+++ Chapter-11 Loops with Pattern/11-2 With Space Pattern/self-work/13.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+main ()
+{
+	int i, j, s;
+	clrscr ();
+	printf("\n\n");
+
+	for (i = 'A'; i <= 'E'; i++)
+	{
+		for (s = 'E'; s > i; s--)
+		{
+			printf("  ");
+		}
+		for (j = i; j >= 'A'; j--)
+		{
+			printf("%c ", j);
+		}
+		printf("\n");
+	}
+
+	getch ();
+}
