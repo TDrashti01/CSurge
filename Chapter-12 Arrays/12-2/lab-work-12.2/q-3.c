d3c855f051b12905f0be2c6b2519903d9ebace64
Drashti Thummar <thummard097@gmail.com>
2026-03-19 12:46:23 +0530

    Chapter - 12 Arrays

---
+++ Chapter-12 Arrays/12-2/lab-work-12.2/q-3.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+main ()
+{
+    int row, col;
+
+    printf("Enter row size :");
+    scanf("%d", &row);
+    
+    printf("Enter column size :");
+    scanf("%d", &col);
+
+    int a [row] [col];
+    int sum = 0;
+
+    printf("\n\nEnter array's elements :\n\n");
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            printf("Enter a [%d] [%d] :", i, j);
+            scanf("%d", &a[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (i + j == row - 1)
+                printf("%d", a[i][j]);
+            
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (i + j == row - 1)
+            {
+                sum += a[i][j];
+            }
+        }
+    }
+    
+    printf("\n\n");
+    printf("Sum of anti-diagonal elements of an Array : %d", sum);
+    
+}
